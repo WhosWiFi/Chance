f0ea385113351dd895a8cb7aa8c6910374c90820
WhosWiFi <whoswifi@gmail.com>
2024-09-26 21:31:31 -0400

    start of app

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+const path = require('path');
+var bodyParser = require('body-parser');
+
+// Middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    fs.readFile('index.html', function (err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      });
+});
+
+app.listen(3000, function () {
+  console.log('Chance is being hosted at http://localhost:3000');
+});
