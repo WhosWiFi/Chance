e893e20b467cc5a8c8a6914b016f79705684769d
WhosWiFi <whoswifi@gmail.com>
2024-09-29 09:23:08 -0400

    usernames now

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
+    fs.readFile('username.html', function (err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      });
+});
+
+app.get('/home', function (req, res) {
     fs.readFile('index.html', function (err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
